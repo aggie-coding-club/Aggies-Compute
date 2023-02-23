fbc37bb54e79a12209dd1d20157caccc2b83bac8
JasonIsAzn <jasonisazn@tamu.edu>
2023-02-22 23:44:13 -0600

    file creation and filler

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Navbar() {
+  //// Testing Functino Portablity. Remove this.
+  return (
+    <div>
+      <p>Navbar Page</p>
+    </div>
+  );
+}
