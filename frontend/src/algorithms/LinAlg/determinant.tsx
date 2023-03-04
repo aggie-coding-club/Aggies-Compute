f36193a01c34f4bbe1b2578f740f0c844fe9b4cd
MARCKGREEN38 <marcvf38@gmail.com>
2023-03-04 15:01:49 -0600

    added function declaration

--- frontend/src/algorithms/LinAlg/determinant.tsx
+++ frontend/src/algorithms/LinAlg/determinant.tsx
@@ -1,6 +1,7 @@
 import 'mathjs';
 import { frac_mat_toString } from './helper_LinAlg';
 
+
 /**
  * Methods to find determinant
  *  For 2x2:
@@ -15,3 +16,18 @@ import { frac_mat_toString } from './helper_LinAlg';
  *  Diagnol method:
  *      1. (a11)(a22)(a33)+(a12)(a23)(a31)+()
  */
+
+//const matrix = [[1, 2], [3, 4]];
+
+function det_2_by_2(a: number, b: number) {
+    return 0;
+  }
+
+function det_3_by_3(a: number, b: number) {
+    return 0;
+  }
+
+function cofactor(a: number, b: number){
+    return 0;
+}
+
