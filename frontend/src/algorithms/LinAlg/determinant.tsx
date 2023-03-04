0008d281bc4a1ad5f66c11e62ef1ae4d55a28114
Ilan Sela <ilansela42@gmail.com>
2023-03-04 15:05:48 -0600

    added more comments

--- frontend/src/algorithms/LinAlg/determinant.tsx
+++ frontend/src/algorithms/LinAlg/determinant.tsx
@@ -13,5 +13,15 @@ import { frac_mat_toString } from './helper_LinAlg';
  * 
  * Algorithm:
  *  Diagnol method:
- *      1. (a11)(a22)(a33)+(a12)(a23)(a31)+()
+ *      1. (a11)(a22)(a33)+(a12)(a23)(a31)+(a13)(a21)(a32)
+ *          -(a31)(a22)(a13)-(a32)(a23)(a11)-(a33)(a21)(a12)
+ *  Co-factor nxn:
+ *      1. Determine row or column with most zeroes
+ *      2. Set pivot on that row/column
+ *      3. If row/column is odd, start with +
+ *            If row/column is even, start with -
+ *      4. 1) Loop rows, skip pivot row
+ *         2) Loop columns, skip pivot column
+ *         Create a new matrix from resultant values
+ *      5. 
  */
