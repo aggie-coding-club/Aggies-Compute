3d3b65d9b6d04bbe02e88605e9eac1502f18ae68
JasonIsAzn <jasonisazn@tamu.edu>
2023-03-04 01:46:34 -0600

    saving again afte rebase

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,30 +1,90 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './nav_styles.css';
 
 export default function Navbar() {
-  // Navigation
-  const navigate = useNavigate();
-  const goCalculator = () => {
-    navigate('/calculator');
-  };
-
-  const goAC_Bot = () => {
-    navigate('/ac_bot');
-  };
-
-  const goPhotoMathClone = () => {
-    navigate('/photomathclone');
-  };
-
-  const goProfile = () => {
-    navigate('/profile');
-  };
-
   return (
     <div>
-      <nav className="navbar-container"></nav>
+      <nav className="navbar-container">
+        <NavLink
+          to="/Landing"
+          style={({ isActive }) => ({
+            color: isActive ? '#fff' : '#fff',
+            textDecoration: 'none'
+          })}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            color: isActive ? '#fff' : '#fff',
+            textDecoration: 'none'
+          })}
+        >
+          ChatGPT
+        </NavLink>
+
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            color: isActive ? '#fff' : '#fff',
+            textDecoration: 'none'
+          })}
+        >
+          PhotoMath
+        </NavLink>
+
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            color: isActive ? '#fff' : '#fff',
+            textDecoration: 'none'
+          })}
+        >
+          Education
+        </NavLink>
+
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            color: isActive ? '#fff' : '#fff',
+            textDecoration: 'none'
+          })}
+        >
+          Profile
+        </NavLink>
+      </nav>
     </div>
   );
 }
+// export default function Navbar() {
+//   // Navigation
+//   const navigate = useNavigate();
+//   const goCalculator = () => {
+//     navigate('/calculator');
+//   };
+
+//   const goAC_Bot = () => {
+//     navigate('/ac_bot');
+//   };
+
+//   const goPhotoMathClone = () => {
+//     navigate('/photomathclone');
+//   };
+
+//   const goProfile = () => {
+//     navigate('/profile');
+//   };
+
+//   return (
+//     <div>
+//       <nav className="navbar-container">
+
+//       </nav>
+//     </div>
+//   );
+// }
