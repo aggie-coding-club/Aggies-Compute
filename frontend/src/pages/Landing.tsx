009fffd840fe6966303db81e640d3044261db05b
JasonIsAzn <jasonisazn@tamu.edu>
2023-03-07 16:02:16 -0600

    started eigen-value-vector, added algebrite dependency

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { gauss_elimination } from '../algorithms/LinAlg/rref';
+import { eigen_value_vector } from '../algorithms/LinAlg/eigen_value_vector';
 import 'mathjs';
 import NavBar from '../components/Navbar';
 var math = require('mathjs');
 export default function Landing() {
-
+  console.log(eigen_value_vector());
   return (
     <div className="">
       <h1 className="underline text-red-600">landing page</h1>
