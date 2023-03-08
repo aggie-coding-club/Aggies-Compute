5e5b9eafee3627ec749b44df0bac0954d100b74f
JasonIsAzn <jasonisazn@tamu.edu>
2023-03-08 00:52:42 -0600

    temp navigation between pages

--- frontend/src/pages/Algebra/AlgebraLanding.tsx
+++ frontend/src/pages/Algebra/AlgebraLanding.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function AlgebraLanding() {
   return (
     <div>
-      <p>Algrebra Landing Page</p>
+      <ul className="flex flex-col items-center justify-center m-[5%]">
+        <NavLink
+          to="/algebra"
+          className="bg-[#27476e] text-white p-[1%] my-[1%] rounded-md"
+        >
+          replace-this
+        </NavLink>
+      </ul>
     </div>
   );
 }
