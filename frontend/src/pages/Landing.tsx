09d1633c1756ce3b5bfe2a6add79da4484a46ed1
JasonIsAzn <jasonisazn@tamu.edu>
2023-03-08 00:52:42 -0600

    temp navigation between pages

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { gauss_elimination } from '../algorithms/LinAlg/rref';
 import { NavLink } from 'react-router-dom';
-
-import 'mathjs';
-import NavBar from '../components/Navbar';
-var math = require('mathjs');
 export default function Landing() {
   return (
     <div className="">
-      <ul className="flex flex-col">
-        <NavLink to="/algebra">Algebra Pages</NavLink>
-        <NavLink to="/calculus">Calculus Pages</NavLink>
-        <NavLink to="/crypto">Crypto Pages</NavLink>
-        <NavLink to="/linalg">Linear Algebra Pages</NavLink>
+      <ul className="flex flex-col items-center justify-center m-[5%]">
+        <NavLink
+          to="/algebra"
+          className="bg-[#27476e] text-white p-[1%] my-[1%] rounded-md"
+        >
+          Algebra Pages
+        </NavLink>
+        <NavLink
+          to="/calculus"
+          className="bg-[#27476e] text-white p-[1%] my-[1%] rounded-md"
+        >
+          Calculus Pages
+        </NavLink>
+        <NavLink
+          to="/crypto"
+          className="bg-[#27476e] text-white p-[1%] my-[1%] rounded-md"
+        >
+          Crypto Pages
+        </NavLink>
+        <NavLink
+          to="/linalg"
+          className="bg-[#27476e] text-white p-[1%] my-[1%] rounded-md"
+        >
+          Linear Algebra Pages
+        </NavLink>
       </ul>
     </div>
   );
