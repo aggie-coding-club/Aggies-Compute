73e16e25fedfc72c6125c61f37e8995f45c09836
JasonIsAzn <jasonisazn@tamu.edu>
2023-03-13 23:09:00 -0500

    making color variables

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -5,18 +5,18 @@ import Navbar from '../components/Navbar';
 
 export default function Landing() {
   return (
-    <div className="bg-[#FAFAFA]">
+    <div className="bg-bg_grey">
       <div className="sticky top-0 z-50">
         <Navbar isLanding={false} />
       </div>
 
       {/* landing-page-header */}
-      <div className="flex flex-col w-full h-56 items-center w-full bg-gradient-to-b from-[#27476e] to-[#37C4C4] via-[#006992] animate-gradient-y ">
-        <h1 className="flex items-center w-full h-1/3 pt-6 justify-center text-6xl font-extrabold  text-[#44f2f2]">
-          Aggie <span className="text-[#00b3f8]">Compute</span>
+      <div className="flex flex-col w-full h-56 items-center w-full bg-gradient-to-b from-primary_blue-dark to-primary_blue via-primary_blue animate-gradient-y ">
+        <h1 className="flex items-center w-full h-1/3 pt-6 justify-center text-6xl font-extrabold  text-primary_blue-light-2">
+          Aggie <span className="text-primary_blue-dark">Compute</span>
         </h1>
 
-        <h2 className="flex flex-col items-center w-full pt-6 h-2/3 justify-center text-xl font-medium  text-white">
+        <h2 className="flex flex-col items-center w-full pt-6 h-2/3 justify-center text-xl font-medium text-white">
           Empowering Students with Math:
           <span>
             <span className="text-[#37C4C4] font-bold"> Free </span> and
@@ -38,57 +38,42 @@ export default function Landing() {
           >
             <img
               className="rounded-lg h-full w-full object-cover"
-              src="https://media.giphy.com/media/l2Je3qSgOVvFPdaNi/giphy.gif"
+              src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmY5YzU0MjNiOWYyYmM4MWY5OGNkNGU4ZDVmZWRkNDJkZWNkNWEwZSZjdD1n/VbnUQpnihPSIgIXuZv/giphy.gif"
             ></img>
           </NavLink>
           <h1 className="flex items-center justify-center font-bold text-lg mt-5">
             Algebra
           </h1>
         </div>
-        <div className="flex flex-col">
-          <NavLink
-            to="/calculus"
-            className="bg-[#27476e] text-2xl text-white rounded-md h-full w-full flex items-center justify-center"
-          >
-            <img
-              className="rounded-lg h-full w-full object-cover"
-              src="https://media.giphy.com/media/l2YWF00ZX8wOs0p0s/giphy.gif"
-            ></img>
-          </NavLink>
-          <h1 className="flex items-center justify-center font-bold text-lg mt-5">
-            Calculus
-          </h1>
-        </div>
 
-        <div className="flex flex-col">
-          <NavLink
-            to="/crypto"
-            className="bg-[#27476e] text-2xl text-white rounded-md h-full w-full flex items-center justify-center"
-          >
-            <img
-              className="rounded-lg h-full w-full object-cover"
-              src="https://media.giphy.com/media/5xtDarJ7d5HXTRULbSo/giphy.gif"
-            ></img>
-          </NavLink>
-          <h1 className="flex items-center justify-center font-bold text-lg mt-5">
-            Cryptography
-          </h1>
-        </div>
+        <NavLink
+          to="/calculus"
+          className="bg-[#27476e] text-2xl text-white rounded-md h-full w-full flex items-center justify-center"
+        >
+          <img
+            className="rounded-lg h-full w-full object-cover"
+            src="https://media.giphy.com/media/l2YWF00ZX8wOs0p0s/giphy.gif"
+          ></img>
+        </NavLink>
+        <NavLink
+          to="/crypto"
+          className="bg-[#27476e] text-2xl text-white rounded-md h-full w-full flex items-center justify-center"
+        >
+          <img
+            className="rounded-lg h-full w-full object-cover"
+            src="https://media.giphy.com/media/xjTIBA4gjvXIQ/giphy.gif"
+          ></img>
+        </NavLink>
 
-        <div className="flex flex-col">
-          <NavLink
-            to="/linalg"
-            className="bg-[#27476e] text-2xl text-white rounded-md h-full w-full flex items-center justify-center"
-          >
-            <img
-              className="rounded-lg h-full w-full object-cover"
-              src="https://media.giphy.com/media/OsMRCAQICXU8j8j7LI/giphy.gif"
-            ></img>
-          </NavLink>
-          <h1 className="flex items-center justify-center font-bold text-lg mt-5">
-            Linear Algebra
-          </h1>
-        </div>
+        <NavLink
+          to="/linalg"
+          className="bg-[#27476e] text-2xl text-white rounded-md h-full w-full flex items-center justify-center"
+        >
+          <img
+            className="rounded-lg h-full w-full object-cover"
+            src="https://media.giphy.com/media/OsMRCAQICXU8j8j7LI/giphy.gif"
+          ></img>
+        </NavLink>
       </div>
 
       {/* <ul className="flex flex-col items-center justify-center m-[5%]">
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      bg_grey: '#FAFAFA',
+      primary_blue: {
+        'light-2': '#44f2f2',
+        'light-1': '#00b3f8',
+        'light-0': '#37C4C4',
+        DEFAULT: '#006992',
+        dark: '#27476e'
+      }
+    },
     extend: {
       animation: {
         'gradient-x': 'gradient-x 15s ease infinite',
