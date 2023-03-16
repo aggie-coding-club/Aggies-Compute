c445e0353d298b7f12b2ae0d52fea6ad09b97996
JasonIsAzn <jasonisazn@tamu.edu>
2023-03-16 01:31:39 -0500

    feature of landing page mostly complete

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -5,7 +5,7 @@ import './Landing.css';
 
 export default function Landing() {
   return (
-    <div className="bg-bg_grey">
+    <div className="bg-bg_grey overflow-x-auto">
       <div className="">
         <Navbar isLanding={true} />
       </div>
@@ -30,9 +30,6 @@ export default function Landing() {
       </div>
 
       <div className="m-12 pb-12">
-        {/* <div className="flex flex-col items-center justify-center pb-12">
-          <h1 className="text-4xl font-bold">Subjects</h1>
-        </div> */}
         {/* later may need to dymaically determine row # */}
         <div className="grid grid-cols-3 grid-rows-2 grid gap-14 px-64 pb-12">
           <div className="flex flex-col">
@@ -50,7 +47,7 @@ export default function Landing() {
               <h1 className="flex items-center w-full justify-center font-bold text-lg mt-5">
                 Algebra
               </h1>
-              <h1 className="text-center text-slate-400">
+              <h1 className="text-center italic	text-slate-400">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </h1>
             </div>
@@ -70,7 +67,7 @@ export default function Landing() {
               <h1 className="flex items-center w-full justify-center font-bold text-lg mt-5">
                 Calculus
               </h1>
-              <h1 className="text-center text-slate-400">
+              <h1 className="text-center italic text-slate-400">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </h1>
             </div>
@@ -91,7 +88,7 @@ export default function Landing() {
               <h1 className="flex items-center w-full justify-center font-bold text-lg mt-5">
                 Cryptography
               </h1>
-              <h1 className="text-center text-slate-400">
+              <h1 className="text-center italic	text-slate-400">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </h1>
             </div>
@@ -113,7 +110,7 @@ export default function Landing() {
               <h1 className="flex items-center w-full justify-center font-bold text-lg mt-5">
                 Linear Algebra
               </h1>
-              <h1 className="text-center text-slate-400">
+              <h1 className="text-center italic	text-slate-400">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </h1>
             </div>
@@ -135,57 +132,123 @@ export default function Landing() {
               <h1 className="flex items-center w-full justify-center font-bold text-lg mt-5">
                 ...
               </h1>
-              <h1 className="text-center text-slate-400">
+              <h1 className="text-center italic	 text-slate-400">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
               </h1>
             </div>
           </div>
         </div>
 
-        <div className="flex flex-col items-center w-screen h-screen justify-center pt-36">
-          <div className="grid grid-cols-3 w-[70%] h-[70%] border border-2 bg-blue-200">
-            <div className="">
-              <NavLink to="#">
-                <div className="bg-white flex items-center justify-center h-[92%] m-[4%] mr-0 border-4 border-slate-300  ">
-                  <img
-                    className="h-full w-full object-cover"
-                    src={require('../images/chat_bot.jpg')}
-                    alt="chat bot"
-                  ></img>
-                  {/* <h1 className="text-2xl font-bold text-center">
-                    Pratice Problem with AC Chat
-                  </h1> */}
+        <div className="flex flex-col items-center h-screen justify-center pt-16">
+          <div className="grid grid-cols-3 w-[70%] h-[70%] bg-primary_blue-dark">
+            <NavLink
+              to="#"
+              className="relative flex items-center justify-center circle-item"
+            >
+              <div className="absolute bg-white flex items-center justify-center h-[92%] m-[4%] mr-0 rounded-md">
+                <img
+                  className="h-full w-full object-cover rounded-md"
+                  src={require('../images/chat_bot.jpg')}
+                  alt="chat bot"
+                ></img>
+              </div>
+              <div className=" flex text-center items-center justify-center">
+                <div className="relative h-56 w-56 circle-item rounded-full ">
+                  <div className="bg-primary_blue-light-1 w-full h-full rounded-full opacity-0 circle-info">
+                    <h3 className="pt-20 h-32 text-white text-3xl font-bold text-center drop-shadow">
+                      AC BOT
+                    </h3>
+                    <p className="text-white py-[10px] px-[5px] mx-[40px] text-sm border-t-2 border-black border-opacity-25 italic circle-item">
+                      The Intelligent Learning Companion
+                    </p>
+                  </div>
                 </div>
-              </NavLink>
-            </div>
+              </div>
+            </NavLink>
 
-            <div className="">
-              <NavLink to="#">
-                <div className=" bg-white flex items-center justify-center w-[96%] h-[20%] m-[4%] mr-0 border-4 border-slate-300">
+            <div className="grid grid-rows-5 gap-y-[2%] my-[2%]">
+              <NavLink to="#" className="row-span-1">
+                <div className=" bg-bg_grey flex items-center justify-center w-[96%] h-[100%] m-[4%] mr-0 rounded-md">
                   <h1 className="link link-underline link-underline-black text-2xl font-bold text-center">
                     Features
                   </h1>
                 </div>
               </NavLink>
 
-              <NavLink to="#">
-                <div className="bg-white flex items-center justify-center h-[70%] m-[4%] mr-0  border-4 border-slate-300">
-                  <h1 className="text-2xl font-bold text-center ">MATH OCR</h1>
+              <NavLink
+                to="#"
+                className="row-span-4 relative flex items-center justify-center circle-item"
+              >
+                <div className="absolute bg-white flex items-center justify-center h-[92%] m-[4%] mr-0 rounded-md">
+                  <img
+                    className="h-full w-full object-cover rounded-md"
+                    src={require('../images/ocr_feature.jpg')}
+                    alt="math ocr"
+                  ></img>
+                </div>
+                <div className=" flex text-center  items-center justify-center">
+                  <div className="relative h-56 w-56 circle-item rounded-full ">
+                    <div className="bg-primary_blue-light-1 w-full h-full rounded-full opacity-0 circle-info">
+                      <h3 className="pt-20 h-32 text-white text-3xl font-bold text-center drop-shadow">
+                        MATH OCR
+                      </h3>
+                      <p className="text-white py-[10px] px-[5px] mx-[40px] text-sm border-t-2 border-black border-opacity-25 italic circle-item">
+                        The Intelligent Learning Companion
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </NavLink>
             </div>
 
-            <div className="">
-              <NavLink to="#">
-                <div className="bg-white flex items-center justify-center h-[45%]  m-[4%]  border-4 border-slate-300">
-                  <h1 className="text-2xl font-bold text-center ">
-                    Calculator
-                  </h1>
+            <div className="grid grid-rows-2  gap-y-[2%] my-[2%]">
+              <NavLink
+                to="#"
+                className="row-span-1 relative flex items-center justify-center circle-item mx-[4%] my-[4%] mb-0"
+              >
+                <div className="absolute bg-white flex items-center justify-center  h-full  w-full rounded-md">
+                  <img
+                    className="h-full w-full object-cover rounded-md"
+                    src={require('../images/iStock-mathproblems-2048x1366.webp')}
+                    alt="calculator"
+                  ></img>
+                </div>
+                <div className=" flex text-center  items-center justify-center">
+                  <div className="relative h-40 w-40 circle-item rounded-full ">
+                    <div className="bg-primary_blue-light-1 w-full h-full rounded-full opacity-0 circle-info">
+                      <h3 className="pt-10 h-18 text-white text-2xl font-bold text-center drop-shadow">
+                        Calculator
+                      </h3>
+                      <p className="text-white py-[5px] px-[10px] mx-[20%] text-sm border-t-2 border-black border-opacity-25 italic circle-item">
+                        Perform Basic Computations
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </NavLink>
-              <NavLink to="#">
-                <div className="bg-white flex items-center justify-center h-[45%] m-[4%]  border-4 border-slate-300">
-                  <h1 className="text-2xl font-bold text-center ">Filler</h1>
+
+              <NavLink
+                to="#"
+                className="row-span-1 relative flex items-center justify-center circle-item mx-[4%] my-[4%] mt-0"
+              >
+                <div className="absolute bg-white flex items-center justify-center h-full  w-full rounded-md">
+                  <img
+                    className="h-full w-full object-cover rounded-md"
+                    src={require('../images/1.jpg')}
+                    alt="other"
+                  ></img>
+                </div>
+                <div className=" flex text-center  items-center justify-center">
+                  <div className="relative h-40 w-40 circle-item rounded-full ">
+                    <div className="bg-primary_blue-light-1 w-full h-full rounded-full opacity-0 circle-info">
+                      <h3 className="pt-10 h-18 text-white text-2xl font-bold text-center drop-shadow">
+                        Duck
+                      </h3>
+                      <p className="text-white py-[5px] px-[10px] mx-[20%] text-sm border-t-2 border-black border-opacity-25 italic circle-item">
+                        Lorem, ipsum dolor.
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </NavLink>
             </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
   theme: {
     extend: {
       colors: {
@@ -52,5 +55,5 @@ module.exports = {
       }
     }
   },
-  plugins: []
+  plugins: [require('tw-elements/dist/plugin')]
 };
