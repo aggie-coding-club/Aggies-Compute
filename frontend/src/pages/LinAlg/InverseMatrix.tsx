e8465ff95e2e286dc6242d5cf6aba6ed155e5726
Sriram Gaddam <sriramgaddam5@gmail.com>
2023-03-17 01:47:08 -0500

    Update UI again

--- frontend/src/pages/LinAlg/InverseMatrix.tsx
+++ frontend/src/pages/LinAlg/InverseMatrix.tsx
@@ -1,80 +1,100 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
 import Sidebar_Linalg from '../../components/Sidebar_Linalg';
+import { Text, Input } from "@nextui-org/react";
 
 export default function InverseMatrix() {
 
   return (
-    <div className="flex">
-      <Sidebar_Linalg />
-      <div className="flex flex-col w-[60%] mt-20">
-        {/* Title */}
-        <div className="flex items-center justify-center">
-          <p className="text-xl font-bold my-5">
-            Inverse Matrix
-          </p>
-        </div>
-        {/* Description */}
-        <div>
-          <p className="text-lg font-bold my-5">
-            Description
-          </p>
-          <p>
-            Returns the inverse matrix from a given matrix
-          </p>
+    <div className="">
+      <Navbar isLanding={false} />
+      <div className="flex">
+        <Sidebar_Linalg />
+        <div className="flex flex-col w-[60%] mt-20">
 
-        </div>
-        {/* Inputs */}
-        <div>
-          <p className="text-lg font-bold my-5">
-            Inputs
-          </p>
-          <p>
-            Enter your matrix
-          </p>
-          <div className="grid grid-cols-3 gap-4 my-5">
-            <input type="number" id="input" className="bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
-            <input type="number" id="input" className="bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
-            <input type="number" id="input" className="bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
-            <input type="number" id="input" className="bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
-            <input type="number" id="input" className="bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
-            <input type="number" id="input" className="bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
-            <input type="number" id="input" className="bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
-            <input type="number" id="input" className="bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
-            <input type="number" id="input" className="bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+          {/* Title */}
+          <div className="flex items-center justify-center rounded-xl">
+            <Text h1 size={60} css={{ textGradient: "0deg, #F0F0F0, #27476E 50%" }} weight="bold">
+              Inverse Matrix
+            </Text>
+          </div>
+
+          {/* Description */}
+          <div>
+            <h1 className="text-lg font-bold my-5">
+              Description
+            </h1>
+            <p>
+              Returns the inverse matrix from a given matrix
+            </p>
+          </div>
+
+          {/* Inputs */}
+          <div className="bg-[#DCEFF0] p-5 mt-5">
+            <h1 className="text-lg font-bold my-5">
+              Inputs
+            </h1>
+            <div className="flex space-x-5">
+              <p>
+                Matrix size:
+              </p>
+              <input type="number" id="input" min="1" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="3" title="Enter a width" required/>
+              {/* <Input type="number" rounded css={{ $$inputColor: "#ffffff" }} initialValue="3" title="Enter a width" shadow={false} required/> */}
+              <p>
+                X
+              </p>
+              <input type="number" id="input" min="1" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="3" title="Enter a length" required/>
+            </div>
+            < br/>
+            <p>
+              Enter your matrix
+            </p>
+            <div className="grid grid-cols-3 gap-4 my-5">
+              <input type="number" id="input" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+              <input type="number" id="input" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+              <input type="number" id="input" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+              <input type="number" id="input" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+              <input type="number" id="input" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+              <input type="number" id="input" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+              <input type="number" id="input" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+              <input type="number" id="input" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+              <input type="number" id="input" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="0" title="Enter a number" required/>
+            </div>
+          </div>
+
+          {/* Solutions */}
+          <div className="bg-[#AEECEF]/60 p-5">
+            <h1 className="text-lg font-bold my-5">
+              Solution
+            </h1>
+            <div className="grid grid-rows-3 grid-cols-3 gap-4 my-5">
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+            </div>
+          </div>
+
+          {/* Steps */}
+          <div className="bg-[#AAEEEE]/80 p-5 mb-20">
+            <h1 className="text-lg font-bold my-5">
+              Steps
+            </h1>
+            <p>
+              Use the formula
+            </p>
           </div>
         </div>
-        {/* Solutions */}
-        <div>
-          <p className="text-lg font-bold my-5">
-            Solution
-          </p>
-          <div className="grid grid-cols-3 gap-4 my-5">
-            <div className="bg-gray-100 rounded-xl p-2.5 text-gray-900">1</div>
-            <div className="bg-gray-100 rounded-xl p-2.5 text-gray-900">2</div>
-            <div className="bg-gray-100 rounded-xl p-2.5 text-gray-900">3</div>
-            <div className="bg-gray-100 rounded-xl p-2.5 text-gray-900">4</div>
-            <div className="bg-gray-100 rounded-xl p-2.5 text-gray-900">5</div>
-            <div className="bg-gray-100 rounded-xl p-2.5 text-gray-900">6</div>
-            <div className="bg-gray-100 rounded-xl p-2.5 text-gray-900">7</div>
-            <div className="bg-gray-100 rounded-xl p-2.5 text-gray-900">8</div>
-            <div className="bg-gray-100 rounded-xl p-2.5 text-gray-900">9</div>
-          </div>
+        <div className="flex bg-[#DEDEDE] w-[20%] m-16 items-center justify-center">
+          <h1 className="text-xl font-bold">
+            Right Side
+          </h1>
         </div>
-        {/* Steps */}
-        <div>
-          <p className="text-lg font-bold my-5">
-            Steps
-          </p>
-          <p>
-            Use the formula
-          </p>
-        </div>
-      </div>
-      <div className="flex bg-[#DEDEDE] w-[20%] m-16 items-center justify-center">
-        <h1 className="text-xl font-bold">
-          Right Side
-        </h1>
       </div>
     </div>
   );
