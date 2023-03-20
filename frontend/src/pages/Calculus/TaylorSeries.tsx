74757e3255d69666f69638b7adad5ed0e44db2f8
Jackson Garner <requiem07@tamu.edu>
2023-03-20 17:29:46 -0500

    Changed placeholder

--- frontend/src/pages/Calculus/TaylorSeries.tsx
+++ frontend/src/pages/Calculus/TaylorSeries.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function TaylorSeries() {
     return (
         <h1>
-            Taylor Series
+            Taylor Series Placeholder
         </h1>
     );
 }
