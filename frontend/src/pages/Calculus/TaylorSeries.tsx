0d6fe190132e28249991194271c754a2d4b0d7b0
Edgelozo <100235249+Edgelozo@users.noreply.github.com>
2023-03-25 14:20:38 -0500

    Updated TaylorSeries.tsx
    Updated it from a placeholder v1 to placeholder v2, still need to work with sidebar.

--- frontend/src/pages/Calculus/TaylorSeries.tsx
+++ frontend/src/pages/Calculus/TaylorSeries.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
+import SidebarLinalg from '../../components/SidebarLinAlg'; //Has not been created for Calculus
 
 export default function TaylorSeries() {
-    return (
-        <h1>
-            Taylor Series Placeholder
-        </h1>
-    );
-}
+  return (
+    <div className="">
+      <Navbar isLanding={false} />
+      <div className="flex">
+        <SidebarLinalg />
+        <div className="flex flex-col w-[60%] mt-20">
+          {/* Title */}
+          <div className="flex items-center justify-center">
+            <p className="text-xl font-bold">Taylor / Maclaurin Series</p>
+          </div>
+          {/* Description */}
+          <div>
+            <p>
+              Intro Paragraph
+            </p>
+          </div>
+          {/* Inputs */}
+          <div>
+            <p className='text-lg font-bold my-5'>Input</p>
+          </div>
+          {/* Solutions */}
+          <div>
+            <p className='text-lg font-bold my-5'>Solutions</p>
+          </div>
+          {/* Steps */}
+          <div>
+            <p className='text-lg font-bold my-5'>Steps</p>
+          </div>
+        </div>
+        <div className="flex bg-[#DEDEDE] w-[20%] m-16 items-center justify-center">
+          <h1 className="text-xl font-bold"> Right Side</h1>
+        </div>
+      </div>
+    </div>
+  );
+}   
