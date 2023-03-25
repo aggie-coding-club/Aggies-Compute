a19bb32e476d420eb51ef077351e75b4921b7c0a
Pillber <melcard16@gmail.com>
2023-03-25 14:47:04 -0500

    new template for page

--- frontend/src/pages/Algebra/Pemdas.tsx
+++ frontend/src/pages/Algebra/Pemdas.tsx
@@ -1,74 +1,77 @@
-import React from 'react';
+import React, {SetStateAction, useState} from 'react';
 import Navbar from '../../components/Navbar';
-import SidebarLinalg from '../../components/SidebarLinAlg';
+import Sidebar_Linalg from '../../components/Sidebar_Linalg';
+import { Text } from "@nextui-org/react";
 
-export default function Pemdas() {
-return (
+export default function InverseMatrix() {
+  const [yourOwnVariable, setYourOwnVariable] = useState("");
+  
+  const handleYourOwnVariableChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    setYourOwnVariable(target.value);
+  };
+
+  return (
     <div className="">
-        <Navbar isLanding={false} />
-        <div className="flex">
-        <SidebarLinalg />
+      <Navbar isLanding={false} />
+      <div className="flex">
+        <Sidebar_Linalg />
         <div className="flex flex-col w-[60%] mt-20">
-            {/* Title */}
-            <div className="flex items-center justify-center">
-            <p className="text-xl font-bold">PEMDAS</p>
-            </div>
-            {/* Description */}
-            <div>
+
+          {/* Title */}
+          <div className="flex items-center justify-center rounded-xl">
+            <Text h1 size={60} css={{ textGradient: "0deg, #F0F0F0, #27476E 50%" }} weight="bold">
+              Enter title here
+            </Text>
+          </div>
+
+          {/* Description */}
+          <div>
+            <h1 className="text-lg font-bold my-5">
+              Description
+            </h1>
             <p>
-                PEMDAS is a predetermined series of steps used to strictly organize order of operations. 
-                PEMDAS itself is an acronym for Parenthesis, Exponents, Multiplication, Division, Addition, and Subtraction.
-                The order of operations is meant to be carried out in the same order as the acronym, with a few caveats:
+              Enter description here
             </p>
-            <ul>
-                <li>Parenthesis are evaluated from the lowest level outwards</li>
-                <li>Multiplication and Division are carried out left to right in the order they appear in the equation</li>
-                <li>Addition and Subtraction are carried out left to right in the order they appear in the equation</li>
-            </ul>
-            </div>
-            {/* Inputs */}
-            <div>
-                <p className="text-lg font-bold my-5">Inputs</p>
-                <p>Enter your expression</p>
-                <div className="flex flex_row">
-                    <input
-                        type="text"
-                        id="input"
-                        className="basis-full bg-gray-100 rounded-xl p-2.5 text-black text-center focus:bg-transparent transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none"
-                        placeholder=""
-                        title="Enter the expression"
-                        required
-                    />
-                </div>
-            </div>
-            {/* Calculate */}
-            <div className='flex flex_row justify-center'>
-                <input
-                    type="button"
-                    id="calculate"
-                    className="basis-1/4 bg-blue-600 rounded-xl p-2.5 m-5"
-                    value="Calculate"
-                />
-            </div>
-            {/* Solutions */}
-            <div>
-                <p className="text-lg font-bold my-5">Solution</p>
-                <div className="flex flex-row">
-                    <div className="basis-full bg-gray-100 rounded-xl p-2.5 text-gray-900">
-                        1
-                    </div>
-                </div>
-            </div>
-            {/* Steps */}
-            <div>
-                <p className="text-lg font-bold my-5">Steps</p>
-                <p>Use the formula</p>
-            </div>
+          </div>
+
+          {/* Inputs */}
+          <div className="bg-[#DCEFF0] p-5 mt-5">
+            <h1 className="text-lg font-bold my-5">
+              Inputs
+            </h1>
+            <p>
+              Enter input name here
+            </p>
+            <input type="number" id="input" name="yourOwnVariable" min="1" className="w-full max-w-20 bg-gray-100 rounded-xl p-2.5 text-black focus:bg-transparent focus:outline-none transform: transition duration-100 hover:bg-gray-50 hover:scale-105 motion-reduce:transform-none" placeholder="3" title="Enter a width" onChange={handleYourOwnVariableChange} required/>
+          </div>
+
+          {/* Solutions */}
+          <div className="bg-[#AEECEF]/60 p-5">
+            <h1 className="text-lg font-bold my-5">
+              Solution
+            </h1>
+            <p>
+              Enter solution here
+            </p>
+          </div>
+
+          {/* Steps */}
+          <div className="bg-[#AAEEEE]/80 p-5 mb-20">
+            <h1 className="text-lg font-bold my-5">
+              Steps
+            </h1>
+            <p>
+              Enter steps here
+            </p>
+          </div>
         </div>
         <div className="flex bg-[#DEDEDE] w-[20%] m-16 items-center justify-center">
-            <h1 className="text-xl font-bold"> Right Side</h1>
+          <h1 className="text-xl font-bold">
+            Right Side
+          </h1>
         </div>
-        </div>
+      </div>
     </div>
-    );
+  );
 }
