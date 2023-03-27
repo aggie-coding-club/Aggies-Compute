d45310119e59a1322f9b42f7b7fcf1bfaa30676c
gcarloin2002 <gcarloin2002@gmail.com>
2023-03-27 17:05:28 -0500

    Finished helper CRT function

--- frontend/src/algorithms/Crypto/chineseRemainderTheorem.ts
+++ frontend/src/algorithms/Crypto/chineseRemainderTheorem.ts
@@ -23,20 +23,36 @@ export function CRT_helper(a1: bigint, a2: bigint, p1: bigint, p2: bigint): bigi
         throw Error("gcd(p1,p2) must be 1");
     }
 
-    
+    // If a1 > p1
+    if (a1 > p1){
+        a1 = a1 % p1;
+    }
 
+    // If a2 > p2
+    if (a2 > p2){
+        a2 = a1 % p2;
+    }
 
+    // If a1 is negative
+    if (a1 < BigInt(0)){
+        a1 = p1 - ((a1*BigInt(-1)) % p1);
+    }
 
+    // If a2 is negative
+    if (a2 < BigInt(0)){
+        a2 = p2 - ((a2*BigInt(-1)) % p2);
+    }
 
+    // Calculates k
+    let k: bigint = ((inverseModBigInt(p1, p2)) * (a2 - a1));
 
+    // If k is negative
+    if (k < BigInt(0)){
+        k = p2 - ((k*BigInt(-1)) % p2);
+        k = k % p2;
+    }
 
+    let x: bigint = a1 + (p1*k);
 
-
-
-
-
-
-
-
-    return BigInt(0);
+    return x;
 }
