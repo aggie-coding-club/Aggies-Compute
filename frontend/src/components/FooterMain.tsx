e541a18906cb0a623dbbeadfe4ad6ad8abb54598
JasonIsAzn <jasonisazn@tamu.edu>
2023-04-15 13:31:33 -0500

    landing page for courses

--- frontend/src/components/FooterMain.tsx
+++ frontend/src/components/FooterMain.tsx
@@ -31,7 +31,7 @@ export default function Footer() {
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase text-primary_blue">Resources</h2>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase text-primary_blue">AC Resources</h2>
                             <ul className="text-gray-600 font-medium">
                                 <li className="mb-4">
                                     <a href="#" className="hover:underline ">Calculator List</a>
